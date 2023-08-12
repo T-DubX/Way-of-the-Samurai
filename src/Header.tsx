84ca820984cf66b2f95e798fef113edc04147e7d
Anton Brashkov <spone161@gmail.com>
2023-08-12 13:17:49 +0300

    Prakika first 9 videos of the way of the samurai

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <a href='#'>home</a>
+                <a href='#'>news feed</a>
+                <a href='#'>messages</a>
+            </ul>
+        </div>
+    );
+}
+
+export default Header
