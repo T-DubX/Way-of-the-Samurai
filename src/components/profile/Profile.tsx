2fbbf66633e7a9c8b91a325b6f73a1a1d67414cf
Anton Brashkov <spone161@gmail.com>
2023-09-14 18:14:28 +0300

    styling profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,28 +1,56 @@
 import React from 'react';
 import mainImg from '../../assets/images/2219350.jpg'
+import avatar from '../../assets/images/avatar.jpg'
 import s from './Profile.module.css'
 
 export const Profile = () => {
     return (
         <div className={s.content}>
             <img src={mainImg} alt=""/>
-            <div>
-                avatar + description
+            <div className={s.profileInfo}>
+                <div className={s.wrapperImg}>
+                    <img src={avatar} alt=""/>
+                </div>
+
+                <div className={s.description}>
+                    <span className={s.name}>
+                        Anton B.
+                    </span>
+
+                    <span>
+                        Date of Birth: 27 january
+                    </span>
+                    <span>
+                        City: Osipovichi
+                    </span>
+                    <span>
+                        Education: OGPTK`18
+                    </span>
+                    <span>Web Site: -</span>
+
+                </div>
             </div>
             <div>
                 My posts
             </div>
 
-            <div>
-                New post
+            <div className={s.newPost}>
+                <textarea placeholder='your news...'></textarea>
+                <button className={s.btn}>Send</button>
             </div>
             <div className={s.posts}>
                 <div className={s.item}>
-                    post 1
+                    <div className={s.img}></div>
+                    <p className={s.text}>
+                        Hey, why nobody love me?
+                    </p>
                 </div>
 
                 <div className={s.item}>
-                    post 2
+                    <div className={s.img}></div>
+                    <p className={s.text}>
+                        It`s our new program! Hey!
+                    </p>
                 </div>
             </div>
 
