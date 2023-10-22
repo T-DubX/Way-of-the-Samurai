2e88383edd26f95ec3ac960e268b5b3a2b03422c
Anton Brashkov <spone161@gmail.com>
2023-10-23 00:32:04 +0300

    drawing data using map

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -4,12 +4,12 @@ import {DialogItem} from "./dialogItem/DialogItem";
 import {Message} from "./message/Message";
 
 type DialogsDataType = {
-    id: number
+    id: string
     name: string
 }
 
 type MessageDataType = {
-    id: number
+    id: string
     message: string
 }
 
@@ -17,35 +17,32 @@ type DialogsPropsType = {}
 
 export const Dialogs: FC<DialogsPropsType> = (props) => {
 
-    const dialogsData: DialogsDataType[] = [
-        {id: 1, name: 'Anton'},
-        {id: 2, name: 'Alex'},
-        {id: 3, name: 'Valera'},
-        {id: 4, name: 'Pasha'},
-        {id: 5, name: 'Viktoria'},
-        {id: 6, name: 'Ekaterina'},
+    const dialogs: DialogsDataType[] = [
+        {id: '1', name: 'Anton'},
+        {id: '2', name: 'Alex'},
+        {id: '3', name: 'Valera'},
+        {id: '4', name: 'Pasha'},
+        {id: '5', name: 'Viktoria'},
+        {id: '6', name: 'Ekaterina'},
     ]
 
-    const messageData: MessageDataType[] = [
-        {id: 1, message: 'Hello world'},
-        {id: 2, message: 'I am from Belarus'},
-        {id: 3, message: 'How are you?'},
+    const messages: MessageDataType[] = [
+        {id: '1', message: 'Hello world'},
+        {id: '2', message: 'I am from Belarus'},
+        {id: '3', message: 'How are you?'},
     ]
+
+    const dialogsElements: JSX.Element[] = dialogs.map(dialog => <DialogItem id={dialog.id} name={dialog.name}/>)
+
+    const messagesElements: JSX.Element[] = messages.map(message => <Message message={message.message}/>)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id={'1'} name={'Anton'}/>
-                <DialogItem id={'2'} name={'Alex'}/>
-                <DialogItem id={'3'} name={'Valera'}/>
-                <DialogItem id={'4'} name={'Pasha'}/>
-                <DialogItem id={'5'} name={'Viktoria'}/>
-                <DialogItem id={'6'} name={'Ekaterina'}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={'Hello world'}/>
-                <Message message={'I am from Belarus'}/>
-                <Message message={'How are you?'}/>
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -3,29 +3,32 @@ import s from './MyPosts.module.css'
 import {Post} from "./posts/Post";
 
 type PostsType = {
-    id: number
+    id: string
     message: string
     likesCount: number
 }
 
 export const MyPosts = () => {
 
-    const postsData: PostsType[] = [
-        {id: 1, message: 'It`s our new program! Hey!', likesCount: 12},
-        {id: 1, message: 'It`s my first posts', likesCount: 11},
+    const posts: PostsType[] = [
+        {id: '1', message: 'It`s our new program! Hey!', likesCount: 12},
+        {id: '2', message: 'It`s my first posts', likesCount: 11},
     ]
 
-    return (
-        <div>
-            My posts
+    const postsElements: JSX.Element[] = posts.map(post => <Post message={post.message} likeCount={post.likesCount}/>)
 
+    return (
+        <div className={s.wrapperPosts}>
+            <h3>My posts</h3>
             <div className={s.newPost}>
                 <textarea placeholder='your news...'></textarea>
                 <button className={s.btn}>Send</button>
             </div>
-
-            <Post message={' It`s our new program! Hey!'} likeCount={12}/>
-            <Post message={"It's my first posts"} likeCount={11}/>
+            <div className={s.posts}>
+                {
+                    postsElements
+                }
+            </div>
 
         </div>
     );
