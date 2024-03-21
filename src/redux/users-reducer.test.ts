f93e4d995946d5cae24a9d1949aaca4a454bb8ce
Anton <spone161@gmail.com>
2024-03-22 00:01:41 +0300

    added tests actions users-reducer

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -1,44 +1,59 @@
-import {InitialUsersStateType, setUsers, usersReducer, UserType} from "./users-reducer";
+import {followSuccess, InitialUsersStateType, setUsers, unfollowSuccess, usersReducer, UserType} from "./users-reducer";
 
 let state: InitialUsersStateType
-let users: UserType[]
 beforeEach(() => {
    state = {
-      users: [] as UserType[],
+      users: [{
+         name: 'Pavel',
+         id: 1,
+         uniqueUrlName: '',
+         photos: {
+            small: null,
+            large: null
+         },
+         status: 'I am Pavel',
+         followed: false
+      }, {
+         name: 'Anton',
+         id: 2,
+         uniqueUrlName: null,
+         photos: {
+            small: null,
+            large: null
+         },
+         status: null,
+         followed: false
+      }],
       pageSize: 5,
       totalUsersCount: 0,
       currentPage: 1,
       isFetching: true,
       followingInProgress: [] as number[] | never[]
+
    }
 
-   users = [{
-      name: 'Pavel',
-      id: 10,
-      uniqueUrlName: '',
-      photos: {
-         small: null,
-         large: null
-      },
-      status: 'I am Pavel',
-      followed: false
-   }, {
-      name: 'Anton',
-      id: 1,
-      uniqueUrlName: null,
-      photos: {
-         small: null,
-         large: null
-      },
-      status: null,
-      followed: false
-   }]
 })
 
-it('length should be at data acquisition', () => {
-   const action = setUsers(users)
+test('length should be at data acquisition', () => {
+   const action = setUsers(state.users)
    const newState = usersReducer(state, action)
 
 
    expect(newState.users.length).toBe(2)
+})
+
+test('follow success' , () => {
+   const action = followSuccess(1)
+   const newState = usersReducer(state, action)
+
+   expect(newState.users[0].followed).toBeTruthy()
+   expect(newState.users[1].followed).toBeFalsy()
+})
+
+test('unfollow success' , () => {
+   const action = unfollowSuccess(2)
+   const newState = usersReducer(state, action)
+
+   expect(newState.users[0].followed).toBeFalsy()
+   expect(newState.users[1].followed).toBeTruthy()
 })
